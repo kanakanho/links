7adfb9fa4958fd37a419ca36fb3af85edbe997dd
kanakanho <kahoshibakichi@gmail.com>
2023-09-12 11:14:49 +0900

    remove: コンポーネント

--- src/components/Parts/Main/Margin.jsx
+++
@@ -1,9 +0,0 @@
-import "./margin.css";
-import React from "react";
-
-function Margin(props) {
-    const { margin:text } = props;
-    return <div className={`${text}`}></div>;
-}
-
-export default Margin;
