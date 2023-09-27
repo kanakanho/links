228c7845e580a44ad15eb4b7a6a0c8ecbf57f721
kanakanho <kahoshibakichi@gmail.com>
2023-09-27 09:18:15 +0900

    fix: JSONを直接読み出す形に変更

--- src/components/pages/main.jsx
+++ src/components/pages/main.jsx
@@ -1,15 +1,12 @@
 import "./main.css";
 import React from "react";
 import TextBox from "../templates/main/text-box";
-import { TextBoxProvider } from "../../prosessing/TextBoxContext.jsx";
 
 class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <TextBoxProvider>
-                    <TextBox />
-                </TextBoxProvider>
+                <TextBox />
             </div>
         );
     }
